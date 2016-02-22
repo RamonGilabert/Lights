42eb09a800f99185c099a09521d84e2aea30d00c
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-02-22 23:10:05 +0100

    Adds the first structure for the Xcode project

--- Lights/Lights/AppDelegate.swift
+++ Lights/Lights/AppDelegate.swift
@@ -5,8 +5,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+  lazy var controller: ViewController = ViewController()
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    window?.rootViewController = controller
+    window?.makeKeyAndVisible()
+
     return true
   }
 }
